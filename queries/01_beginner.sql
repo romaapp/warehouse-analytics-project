41d25e141b9e5ea5497b3c7c9939426d74ed23f2
romaapp <romaapp@ya.ru>
2026-07-30 19:48:24 +0300

    Create master data schema for WMS

--- queries/01_beginner.sql
+++ queries/01_beginner.sql
@@ -129,5 +129,181 @@ GROUP BY
 
 ORDER BY
     o.owner_name ASC,
-    total_quantity DESC
+    total_quantity DESC;
+
+
+--7
+
+SELECT
+    tp.package_code,
+    tp.package_type,
+    tp.package_status,
+    tp.block_code,
+    tp.gross_weight,
+    l.address
+
+FROM
+    transport_package AS tp
+
+JOIN
+        location AS l
+ON tp.location_id = l.location_id
+
+WHERE
+    tp.block_code IS NOT NULL
+  AND trim(tp.block_code)<>''
+AND tp.package_status = 'STORED'
+
+ORDER BY
+    tp.block_code ASC,
+    tp.gross_weight DESC;
+
+
+--8
+
+SELECT
+    l.address,
+    tp.package_code,
+    p.sku,
+    p.product_name,
+    s.quantity
+
+FROM
+    stock AS s
+
+JOIN
+        product AS p
+ON p.product_id = s.product_id
+
+JOIN
+        transport_package AS tp
+ON tp.package_id = s.package_id
+
+JOIN
+        location AS l
+ON l.location_id = tp.location_id
+
+WHERE
+    s.quantity>0
+AND tp.package_status = 'STORED'
+ORDER BY
+    l.address ASC,
+    p.sku ASC;
+
+
+--9
+
+SELECT
+    p.sku,
+    p.product_name,
+    s.quantity,
+    s.reserved_qty,
+    (s.quantity-s.reserved_qty) AS available_qty
+
+FROM
+    product AS p
+
+JOIN
+    stock AS s
+ON s.product_id = p.product_id
+
+WHERE
+    s.quantity-s.reserved_qty>0
+
+ORDER BY
+    available_qty DESC;
+
+
+--10
+
+SELECT
+    p.sku,
+    p.product_name,
+    SUM(s.quantity) AS total_quantity,
+    SUM(s.reserved_qty) AS total_reserved,
+    (SUM(s.quantity)-SUM(s.reserved_qty)) AS available_qty
+
+FROM
+    product AS p
+
+JOIN
+    stock AS s
+ON s.product_id = p.product_id
+
+GROUP BY
+    p.sku,
+    p.product_name
+
+HAVING
+    (SUM(s.quantity)-SUM(s.reserved_qty))>0
+
+ORDER BY
+    available_qty DESC;
+
+
+--11
+
+SELECT
+    p.sku,
+    p.product_name,
+    SUM(s.quantity) AS total_quantity
+
+FROM
+    stock AS s
+
+JOIN
+        product AS p
+ON p.product_id = s.product_id
+
+GROUP BY
+    p.sku,
+    p.product_name
+
+HAVING
+    SUM(s.quantity)>500
+
+ORDER BY
+    total_quantity DESC;
+
+
+--12
+
+SELECT
+    p.sku,
+    p.product_name,
+    COALESCE(SUM(s.quantity),0) AS total_quantity
+
+FROM
+    product AS p
+
+LEFT JOIN
+        stock AS s
+ON p.product_id = s.product_id
+
+GROUP BY
+    p.sku,
+    p.product_name
+
+ORDER BY
+    total_quantity DESC;
+
+
+--13
+
+SELECT
+    p.sku,
+    p.product_name
+
+FROM
+    product AS p
+
+LEFT JOIN
+    stock AS s
+    ON p.product_id = s.product_id
+
+WHERE
+    s.stock_id IS NULL
+
+ORDER BY
+    p.sku ASC;
 
